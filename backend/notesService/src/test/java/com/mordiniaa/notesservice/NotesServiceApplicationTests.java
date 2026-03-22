56469ea4f63d9f1fe29a69cdc557a013a796ab06
grzegMaly <160776453+unsieknown@users.noreply.github.com>
2026-03-22 19:10:42 +0100

    Notes Service Setup

---
+++ backend/notesService/src/test/java/com/mordiniaa/notesservice/NotesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mordiniaa.notesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
