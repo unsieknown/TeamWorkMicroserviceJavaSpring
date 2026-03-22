8f9598d621bc3a19a57615434699d9df1ef0ac2b
grzegMaly <160776453+unsieknown@users.noreply.github.com>
2026-03-22 19:10:04 +0100

    User Service Setup

---
+++ backend/userService/src/main/java/com/mordiniaa/userservice/UserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.mordiniaa.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
