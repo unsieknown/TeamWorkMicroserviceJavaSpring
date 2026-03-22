90420fa5b9b30970abacc5b1c33e73026e4df8c9
grzegMaly <160776453+unsieknown@users.noreply.github.com>
2026-03-22 19:11:24 +0100

    Board Service Setup

---
+++ backend/bordService/src/test/java/com/mordiniaa/bordservice/BordServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mordiniaa.bordservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BordServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
