e8d8418a8db53de0603f98a896389258ab2471d1
grzegMaly <160776453+unsieknown@users.noreply.github.com>
2026-03-24 00:24:46 +0100

    RabbitMQ Configs Added

---
+++ backend/userService/src/main/java/com/mordiniaa/userservice/config/rabbitmq/RabbitMQConfig.java
@@ -0,0 +1,38 @@
+package com.mordiniaa.userservice.config.rabbitmq;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class RabbitMQConfig {
+
+    private final RabbitMQProperties rabbitMQProperties;
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public TopicExchange topicExchange() {
+        return ExchangeBuilder.topicExchange(rabbitMQProperties.getExchange())
+                .durable(true)
+                .build();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(messageConverter());
+        template.setBeanName("user-template");
+
+        return template;
+    }
+}
